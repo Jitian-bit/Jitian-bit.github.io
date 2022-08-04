868e86f0d4062954dcb15d0db52af5a6453d435a
Jitian-bit <68652887+Jitian-bit@users.noreply.github.com>
2022-08-04 16:15:57 -0400

    Update index.js

--- flashcards/basic/scripts/index.js
+++ flashcards/basic/scripts/index.js
@@ -1,12 +1,17 @@
+//Declaration for the class question
 class question{
+    // A constructor is a special function used to create an instance of the class
   constructor(question, answer){
     this.question = question;
     this.answer = answer;
   }
 }
 
+// Declare a new array
+// Create three questions onto the flashcards array
 let flashcards = [new question('this is the first question', 'this is the first answer'), new question('this is the second question', 'this is the second answer'),new question('this is the third question', 'this is the third answer')];
 
+// Loop through the array printing out the question and the answer
 let i = 0;
 while (i < flashcards.length) {
   console.log(flashcards[i].question + '' + flashcards[i].answer);
