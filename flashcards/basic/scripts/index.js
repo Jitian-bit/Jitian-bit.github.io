40e98ad52035452577a9e03eab594f9e4b03ec1c
Jitian-bit <68652887+Jitian-bit@users.noreply.github.com>
2022-08-24 21:06:15 -0400

    Update index.js

--- flashcards/basic/scripts/index.js
+++ flashcards/basic/scripts/index.js
@@ -1,5 +1,5 @@
 //Declaration for the class question
-class question{ 
+class question { 
   // A constructor is a special function used to create an instance of the class
   constructor(question, answer){
     this.question = question;
@@ -9,33 +9,38 @@ class question{
 
 // Declare a new array
 // Create three questions onto the flashcards array
-let flashcards = [new question('this is really the first question', 'this is really the first answer'), new question('this is the second question', 'this is the second answer'),new question('this is the third question', 'this is the third answer')];
+let flashcards = [new question('this is the first question', 'this is the first answer'), new question('this is the second question', 'this is the second answer'),new question('this is the third question', 'this is the third answer')];
 
-// Loop through the array printing out the question and the answer
-let i = 0;
-while (i < flashcards.length) {
-  console.log(flashcards[i].question + '' + flashcards[i].answer);
-  i++;
-}
-let j = 0;
-while (j < flashcards.length) {
-  alert(flashcards[j].question);
-  alert(flashcards[j].answer);
-  j++;
-}
-//document.getElementById("question").style.display = "none";
-//document.getElementById("answer").style.display = "none";
-//let counter = 0;
-//while (counter < flashcards.length) {
-//document.getElementById("question").innerHTML = flashcards[counter].question;
-//document.getElementById('question').onclick = function() { // Wait for a click to be detected on p1
-    //document.getElementById("answer").innerHTML = flashcards[counter].answer;  
-// Display a message
-//}
-//counter++;
-}
-//document.getElementById('this is the first question').onmouseover = function() { // Wait for a click to be detected on p1
-//document.write(flashcards[0].answer); // Display a message
-//}
+// print out the question and the answer one by one
+let counter = 0;
+// this status ensures the answer is to the correspondent question
+let answerIsDisplayed = false;
+// print out the question
+document.getElementById("question").innerHTML = flashcards[counter].question;
+document.getElementById('question').onclick = function() { 
+  // Wait for a click to be detected on the question
+    if (answerIsDisplayed === false) {
+      // print out the correspond answer to the question
+    document.getElementById("answer").innerHTML = flashcards[counter].answer; 
+    counter++;
+    // go back to the first question
+    if (counter === flashcards.length) {
+      counter = 0;
+    }
+    // answer display check
+    answerIsDisplayed = true;
+    }
+} 
 
 
+document.getElementById('answer').onclick = function() { 
+  // Wait for a click to be detected on answer
+    if (answerIsDisplayed === true ) {
+      // print out next question when the last answer is displayed
+    document.getElementById("question").innerHTML = flashcards[counter].question;
+    // clean the answer to last question
+    document.getElementById("answer").innerHTML = "";
+    // answer display status check
+    answerIsDisplayed = false;
+    }
+}
